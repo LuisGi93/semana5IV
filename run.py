c2cb271ae434b009f2886508c2a5dd3cc3ada030
LuisGi93 <luisgguijarro9@gmail.com>
2016-11-07 21:12:03 +0100

    Subiendo los fuentes del proyecto de la semana 5

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 # Run a test server.
-port = os.getenv('PORT', '5000')
+port = os.getenv('VCAP_APP_PORT', '5000')
 
 from app import app
-app.run(host='0.0.0.0', port=int(port), debug=True)
+app.run(host='0.0.0.0', port=int(port))
